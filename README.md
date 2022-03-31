08343cec36566bd22bd07ae7149a74eddd98396c
Satya Prakash <satyaprakash@AS-MAC-0234.local>
2022-03-31 10:12:12 +0530

    adding table image

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # kubernetes-assignment
+-> start docker
+-> minikube start 
+-> minikube dashboard
+-> chmod +x ./script-create.sh 
+-> ./script-create.sh
+-> minikube mount ./dags/:/mnt/airflow/dags
+-> kubectl exec -it puckel-deploy-698fd5d574-tff7g -- bash 
+FERNET_KEY=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")
+export FERNET_KEY=$FERNET_KEY
+airflow initdb
+-> kubectl port-forward svc/puckel-service 8080:8080 
--- dags/execution_dag.py
+++ dags/execution_dag.py
@@ -1,7 +1,8 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
 # from utils import fetch_and_store_execution_dag_run
 
 import psycopg2
@@ -17,21 +18,36 @@ def fetch_and_store_execution_dag_run():
         create_query = """create table execution_table as select dag_id, execution_date from dag_run order by execution_date;"""
         cursor.execute(create_query)
         conn.commit()
+        cursor.execute('select * from execution_table')
+        data = cursor.fetchall()
+        print("This is Data Execution time for every DAG runs :- ")
+        for i in data:
+            print(i)
+
     except Exception as e:
         print(e)
     finally:
         conn.close()
 
 default_args = {
-    "owner": "satyaprakash",
+    "owner": "Satyaprakash",
     "depends_on_past": False,
-    "start_date": datetime(2022, 3, 21),
-    "retries": 0,
+    "start_date": datetime(2022, 3, 30),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
 }
 
-with DAG("execution", default_args=default_args, schedule_interval="0 6 * * *", catchup=False) as dag:
-    t1 = DummyOperator(task_id="check_dag")
+dag = DAG("Kubernetes_assignment", default_args=default_args, schedule_interval=timedelta(1))
 
-    t2 = PythonOperator(task_id="fetch_execution_data_and_store", python_callable=fetch_and_store_execution_dag_run)
+t1 = BashOperator(task_id="Welcome", bash_command="echo Hi This is Satyaprakash From Sigmoid", dag=dag)
 
-    t1 >> t2
+t2 = PythonOperator(task_id="fetch_execution_data_and_store", python_callable=fetch_and_store_execution_dag_run, dag=dag)
+
+t1 >> t2
